970c234bc2429d82e20e596f54d994c29ea898a0
R00ty-T00ty <crazy..stuffedanimals@gmail..com>
2020-09-07 22:20:37 -0400

    initial upload
    initial upload

---
+++ cast.php
@@ -0,0 +1,141 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<link href="http://www.praguerace.com/comiccontrol/defaultstyles.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="http://www.praguerace.com/favicon.ico" type="image/x-icon" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="http://www.praguerace.com/favicon.ico" type="image/x-icon">
+<link rel="icon" href="http://www.praguerace.com/favicon.ico" type="image/x-icon">
+
+<title>Prague Race - Cast</title>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59274900-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<link href="http://www.praguerace.com/styles.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+<div id="wrapper">
+	<a id="logo" href="http://www.praguerace.com/"></a>
+    <div id="menu"><a id="about" href="http://www.praguerace.com/about"></a><a id="store" href="http://www.praguerace.com/store"></a><a id="cast" href="http://www.praguerace.com/cast"></a><a href="http://www.praguerace.com/comic/archive" id="archive"></a><a id="tipjar" href="http://www.praguerace.com/tip-jar"></a></div><div id="leaderboard"><iframe id='a50eb971' name='a50eb971' src='http://162.218.115.228/delivery/afr.php?n=a50eb971&amp;zoneid=77&amp;target=_blank&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='728' height='90'><a href='http://162.218.115.228/delivery/ck.php?n=a44b08ab&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://162.218.115.228/delivery/avw.php?zoneid=77&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a44b08ab' border='0' alt='' /></a></iframe>
+<script type='text/javascript' src='http://162.218.115.228/delivery/ag.php'></script></div>
+<div id="ibar"></div>
+<script src="http://www.thehiveworks.com/jumpbar.js"></script>
+    <div id="bottomleft"><div id="tower"><iframe id='ad609beb' name='ad609beb' src='http://162.218.115.228/delivery/afr.php?n=ad609beb&amp;zoneid=76&amp;target=_blank&amp;cb=1439966221' frameborder='0' scrolling='no' width='160' height='600'><a href='http://162.218.115.228/delivery/ck.php?n=aff9c203&amp;cb=1439966221' target='_blank'><img src='http://162.218.115.228/delivery/avw.php?zoneid=76&amp;cb=1439966221&amp;n=aff9c203' border='0' alt='' /></a></iframe>
+<script type='text/javascript' src='http://162.218.115.228/delivery/ag.php'></script></div></div>
+    <div id="bottommid">
+        <div class="bottomtext"><div class="cc-newscontent">
+<h1>Cast</h1><table>
+</table>
+<table width="518" height="292" cellspacing="" cellpadding="" border="0">
+    <tbody>
+        <tr>
+            <td><img width="250" height="282" src="/image/leona.jpg" alt="" /></td>
+            <td>
+            <p><b>Leona</b><br />
+            <br />
+            Leona is a restless young lady who just moved to her very first  apartment with her cat Gabriele. She is trying to find a way to live in  the society making easy money without having to get a real job ever.</p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<table width="517" height="296" cellspacing="" cellpadding="" border="0">
+    <tbody>
+        <tr>
+            <td><img width="250" height="286" src="/image/kolini.jpg" alt="" /></td>
+            <td><b>Colin</b><br />
+            <br />
+            Colin uses most of his free time taking artistic photographs of trash  cans and homeless people. He is afraid of lots of things and all the  things he seems to get disturbed about are drawn to him.</td>
+        </tr>
+    </tbody>
+</table>
+<table width="516" height="292" cellspacing="" cellpadding="" border="0">
+    <tbody>
+        <tr>
+            <td><img width="250" height="282" src="/image/mikopoika.jpg" alt="" /></td>
+            <td><b>Miko</b><br />
+            <br />
+            Miko is nice to everyone. That is why he often gets treated like a  doormat. Sometimes, though rarely, he gets sad about being kicked  around, but he is too nice to do anything about it. Then he makes tons  of food and eats all of it to drown his sorrows and then he feels bad  for eating so much, and the endless cycle of sadness continues.</td>
+        </tr>
+    </tbody>
+</table>
+<table width="516" height="292" cellspacing="" cellpadding="" border="0">
+    <tbody>
+        <tr>
+            <td><img width="250" height="282" src="/image/gabriele.png" alt="" /></td>
+            <td><b>Gabriele</b><br />
+            <br />
+            He likes sleeping on warm surfaces, like laptops for example. Also  sitting on books when someone's trying to read them is his favourite  thing.</td>
+        </tr>
+    </tbody>
+</table>
+<table width="515" height="292" cellspacing="" cellpadding="" border="0">
+    <tbody>
+        <tr>
+            <td><img width="250" height="282" src="/image/toska_zpsc9470c6c.png" alt="" /></td>
+            <td><b>Toska</b><br />
+            <br />
+            Toska is a bit simple but he covers it up by talking over people and  trying to crawl on top of them and squeezing them flat under his huge  body. Has no idea of personal space whatsoever.</td>
+        </tr>
+    </tbody>
+</table>
+<table width="513" height="292" cellspacing="" cellpadding="" border="0">
+    <tbody>
+        <tr>
+            <td><img width="250" height="282" src="/image/sela_zps3a681529.png" alt="" /></td>
+            <td><b>Sela</b><br />
+            <br />
+            Hates everything. Except Fishsticks. Sometimes shows some feeble, dry emotions that she doesn't probably hate Toska as much as others. She has a lot of shady business going on.</td>
+        </tr>
+    </tbody>
+</table>
+<table width="513" height="292" cellspacing="" cellpadding="" border="0">
+    <tbody>
+        <tr>
+            <td><img width="250" height="282" src="/image/kalapuikko_zps64366245.png" alt="" /></td>
+            <td><b>Fishsticks</b><br />
+            <br />
+            Fishsticks always smiles, how nice! But it's actually because of his facial anatomy, so we can never know what truly goes on behind those tiny black eyes.</td>
+        </tr>
+    </tbody>
+</table>
+<table width="512" height="292" cellspacing="" cellpadding="" border="0">
+    <tbody>
+        <tr>
+            <td><img width="250" height="282" src="/image/tahvo_zpse8d25cad.png" alt="" /></td>
+            <td><b>Tahvo</b><br />
+            <br />
+            Tahvo works at the Rehoming Center for the Accidentally Immigrated,  where he spends his days trying to avoid doing anything. Is often  covered in Doorowls.</td>
+        </tr>
+    </tbody>
+</table>
+<table width="512" height="292" cellspacing="" cellpadding="" border="0">
+    <tbody>
+        <tr>
+            <td><img width="250" height="282" src="/image/pam_zpsea8ad5b5.png" alt="" /></td>
+            <td><b>Pam</b><br />
+            <br />
+            Pam is always coming and going and seems to know every citizen in Vinetia. Makes a lot of awful puns.</td>
+        </tr>
+    </tbody>
+</table></div>
+</div>
+    </div>
+    <div id="rightside"><a id="hiveworks" href="http://www.thehiveworks.com/"></a>
+    <div id="socialmedia"><a href="http://leppu.tumblr.com/"></a><a href="rss.php"></a><a href="https://www.patreon.com/praguerace?ty=h"></a><a href="https://twitter.com/petra_nordlund"></a>
+    </div>
+    <div style="clear:both; height:50px; width:300px;"></div>
+    </div>
+</div>
+</body>
+</html>
---
+++ store.php
@@ -0,0 +1,50 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<link href="http://www.praguerace.com/comiccontrol/defaultstyles.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="http://www.praguerace.com/favicon.ico" type="image/x-icon" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="http://www.praguerace.com/favicon.ico" type="image/x-icon">
+<link rel="icon" href="http://www.praguerace.com/favicon.ico" type="image/x-icon">
+
+<title>Prague Race - Store</title>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59274900-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<link href="http://www.praguerace.com/styles.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+<div id="wrapper">
+	<a id="logo" href="http://www.praguerace.com/"></a>
+    <div id="menu"><a id="about" href="http://www.praguerace.com/about"></a><a id="store" href="http://www.praguerace.com/store"></a><a id="cast" href="http://www.praguerace.com/cast"></a><a href="http://www.praguerace.com/comic/archive" id="archive"></a><a id="tipjar" href="http://www.praguerace.com/tip-jar"></a></div><div id="leaderboard"><iframe id='a50eb971' name='a50eb971' src='http://162.218.115.228/delivery/afr.php?n=a50eb971&amp;zoneid=77&amp;target=_blank&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='728' height='90'><a href='http://162.218.115.228/delivery/ck.php?n=a44b08ab&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://162.218.115.228/delivery/avw.php?zoneid=77&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a44b08ab' border='0' alt='' /></a></iframe>
+<script type='text/javascript' src='http://162.218.115.228/delivery/ag.php'></script></div>
+<div id="ibar"></div>
+<script src="http://www.thehiveworks.com/jumpbar.js"></script>
+    <div id="bottomleft"><div id="tower"><iframe id='ad609beb' name='ad609beb' src='http://162.218.115.228/delivery/afr.php?n=ad609beb&amp;zoneid=76&amp;target=_blank&amp;cb=1435119545' frameborder='0' scrolling='no' width='160' height='600'><a href='http://162.218.115.228/delivery/ck.php?n=aff9c203&amp;cb=1435119545' target='_blank'><img src='http://162.218.115.228/delivery/avw.php?zoneid=76&amp;cb=1435119545&amp;n=aff9c203' border='0' alt='' /></a></iframe>
+<script type='text/javascript' src='http://162.218.115.228/delivery/ag.php'></script></div></div>
+    <div id="bottommid">
+        <div class="bottomtext"><div class="cc-newscontent">
+<h1>Store</h1><p style="text-align: center;"><a href="http://hivemill.com/search?q=prague+race"><strong>CLICK HERE</strong></a> to check out all the prints I have on sale! </p>
+<p style="text-align: center;">I also have pillows, phone cases, bags mugs and shirts on sale <a href="http://society6.com/leppu">HERE</a>, but this store will be closed soon.</p>
+<p style="text-align: center;">If you have any ideas what would make a good print or other merchandise, feel free to write a comment or message me on tumblr or somewhere!</p></div>
+</div>
+    </div>
+    <div id="rightside"><a id="hiveworks" href="http://www.thehiveworks.com/"></a>
+    <div id="socialmedia"><a href="http://leppu.tumblr.com/"></a><a href="rss.php"></a><a href="https://www.patreon.com/praguerace?ty=h"></a><a href="https://twitter.com/petra_nordlund"></a>
+    </div>
+    <div style="clear:both; height:50px; width:300px;"></div>
+    </div>
+</div>
+</body>
+</html>
